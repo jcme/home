704f808e4fd20962a2debad17a6b20a8f64743b9
Jamie Holmes <jamie.holmes@tesco.com>
2021-07-19 20:35:35 +0100

    Updates

---
+++ precache-manifest.3480fd9a033ec0ff281946da95a324ac.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfa277962b6dac65867065614b7f51b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "502006d7fc5fe3c64b14",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "488b4df15096aec9f90a",
+    "url": "/static/js/2.5f8788fa.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.5f8788fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "502006d7fc5fe3c64b14",
+    "url": "/static/js/main.5a6c637d.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "f01fa6d6568527e4c2fdd309a3bb3254",
+    "url": "/static/media/jamie-holmes-cv.f01fa6d6.pdf"
+  },
+  {
+    "revision": "b0b6c3f195756cb6b9e73054fb8d1d8c",
+    "url": "/static/media/jamie-holmes.b0b6c3f1.jpg"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
